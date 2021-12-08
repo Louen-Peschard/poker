c9ca2630b927d9a0bdd46dbd02cd209fc33c2b1b
orane <orane.orveillon@outlook.fr>
2021-12-08 22:16:00 +0100

    Encapsulation _keep_playing (Player)

--- testPoker/player.py
+++ testPoker/player.py
@@ -5,7 +5,7 @@ class Player:
         self._cards = []
         self._stack = 5000
         self._number = number
-        self.keep_playing = 1
+        self._keep_playing = 1
         self.value = 0
 
     @property
@@ -15,6 +15,14 @@ class Player:
     @property
     def player_stack(self):
         return self._stack
+
+    @property
+    def keep_playing(self):
+        return self._keep_playing
+
+    @keep_playing.setter
+    def keep_playing(self, value):
+        self._keep_playing = value
 
     def set_stack(self, value):
         self._stack = value
